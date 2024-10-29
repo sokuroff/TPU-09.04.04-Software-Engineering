33165b6b6d30ff9e0f12902981894fee6ba26e44
sokurov <rsokurov2@gmail.com>
2024-10-29 16:37:34 +0700

    Finished 1st part in 3rd lab

--- Semester 1/Machine learning/Lab 3 - Application of cluster analysis. The PLS method/Script.R
+++ Semester 1/Machine learning/Lab 3 - Application of cluster analysis. The PLS method/Script.R
@@ -1,13 +1,10 @@
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager") # устанавливаем менеджер пакетов
-BiocManager::install("mixOmics") # устанавливаем mixOmics
 
 library(mixOmics) # подключаем его
 set.seed(5249) # для воспроизводимости результатов
 
 data(srbct) # Получаем данные small round bull cell tumour
 X <- srbct$gene # используем уровни экспрессии генов как матрицу X
-Y <- srbct$class # используем классы как матрицу Y
+Y <- srbct$class # используем классы как матрицу Yт
 
 dim(X) # проверяем размерность датафрейма X
 
@@ -20,3 +17,14 @@ plot(pca.srbct)  # столбчатый гарфик собственных зн
 
 plotIndiv(pca.srbct, group = srbct$class, ind.names = FALSE,  
           legend = TRUE, title = 'PCA on SRBCT, comp 1 - 2') 
+
+srbct.splsda <- splsda(X, Y, ncomp = 10)  # устанавливаем ncomp равным 10 для сравнения с PCA в дальнейшем
+
+# оцениваем производительность модели 
+perf.splsda.srbct <- perf(srbct.splsda, validation = "Mfold", # кросс-валидация типа MFold
+                          folds = 5, nrepeat = 10, # 5 частей, 10 повторений
+                          progressBar = FALSE, auc = TRUE) # подключаем площадь под кривой (AUC)
+
+# визуализация результатов оценки
+plot(perf.splsda.srbct, col = color.mixo(5:7), sd = TRUE,
+     legend.position = "horizontal")
